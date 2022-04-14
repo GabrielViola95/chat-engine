4e3aed8ee165b9f864845b8dd797300758321804
GabrielViola95 <92824009+GabrielViola95@users.noreply.github.com>
2022-04-14 12:41:06 -0300

    chat styles and plataform

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import { ChatEngine } from 'react-chat-engine';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import "./App.css";
+
+const App = () => {
+    return (
+        <ChatEngine 
+            height="100vh"
+            projectID="80a489ad-2022-454a-bcb9-a7ad0ff992c1"
+            userName="roberto"
+            userSecret="1234567"
+        />
+    )
 }
 
 export default App;
